8ba3484a5fa8764eb6c99e49ce23d2f30eed8f57
Mariusz Cieniewski <mariusz.cieniewski@acaisoft.com>
2018-08-29 09:42:48 +0200

    redeploy

--- src/dwolla/controller.ts
+++ src/dwolla/controller.ts
@@ -1,46 +1,20 @@
 import {
-    Errors,
-    GET,
     Path,
-    PATCH,
-    PathParam,
     POST,
-    Preprocessor,
-    QueryParam,
-    ContextRequest,
-    ServiceContext,
-    DELETE,
-    HttpError,
 } from 'typescript-rest';
 import {BaseController} from '../api';
 import {Logger} from '../logger';
 import {Inject} from 'typescript-ioc';
-import {Profile, ProfileResponse} from '../profile/models';
-import {ProfileService} from '../profile/service';
-import {transaction} from 'objection';
-import {Security, Tags} from 'typescript-rest-swagger';
 import * as dwolla from '../dwolla';
-import {ValidationError} from '../errors';
 import {event} from './index';
 
-// @Security('api_key')
 @Path('/dwolla')
 export class DwollaController extends BaseController {
     @Inject private logger: Logger;
     @Inject private dwollaClient: dwolla.Client;
-    // private service: UserService;
-    // private profileService: ProfileService;
-    //
-    // constructor(@Inject service: UserService, @Inject profileService: ProfileService) {
-    //     super();
-    //     this.service = service;
-    //     this.profileService = profileService;
-    // }
 
     @POST
     @Path('')
-    // @Preprocessor(BaseController.requireAdmin)
-    // @Tags('users')
     async createUser(data) {
         console.log(JSON.stringify(data, null, 2));
         const eventO = event.factory(data);
