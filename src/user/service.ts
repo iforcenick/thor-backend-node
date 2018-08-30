0b91467f5bd74480ee73780808c54a19950fe655
mcieniewski <mariusz.cieniewski@acaisoft.com>
2018-08-30 14:01:00 +0200

    fix (#41)

--- src/user/service.ts
+++ src/user/service.ts
@@ -93,7 +93,7 @@ export class UserService extends db.ModelService<models.User> {
             transactions: builder => {
                 builder
                     .select([
-                        'id',
+                        'transactions.id',
                         'status',
                         'quantity',
                         'transactions.createdAt',
