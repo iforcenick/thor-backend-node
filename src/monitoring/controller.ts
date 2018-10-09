ae42920498dcd418042eb00e3e8e12d4aceb51ad
felix-soft <felixsoft.dev@gmail.com>
2018-10-09 10:33:58 +0200

    THOR-199: monitor endpoint (#141)

--- src/monitoring/controller.ts
+++ src/monitoring/controller.ts
@@ -1,15 +1,46 @@
 import {GET, Path} from 'typescript-rest';
 import {BaseController} from '../api';
 import {Tags} from 'typescript-rest-swagger';
+import {Inject} from 'typescript-ioc';
+import {Logger} from '../logger';
+import {Config} from '../config';
+import * as os from 'os';
+
+const getRepoInfo = require('git-repo-info');
+const osUtils = require('os-utils');
+
 
 @Tags('monitoring')
 @Path('/')
 export class MonitoringController extends BaseController {
+    constructor(@Inject logger: Logger, @Inject config: Config) {
+        super(logger, config);
+    }
+
     @GET
     @Path('')
     async health() {
+        const info = getRepoInfo();
         return {
-            running: true,
-        };
+                build: {
+                    version: this.config.get('build.version'),
+                    branch: info.branch,
+                    commit: info.sha,
+                    tag: info.tag,
+                },
+                resources: {
+                    memory: {
+                        total: osUtils.totalmem(),
+                        free: osUtils.freemem(),
+                        freePercent: osUtils.freememPercentage(),
+                        usage: process.memoryUsage(),
+                    },
+                    cpu: os.cpus(),
+                    systemUptime: osUtils.sysUptime(),
+                    processUptime: osUtils.processUptime(),
+                    averageLoad: osUtils.loadavg(1),
+                },
+                running: true,
+            };
     }
 }
