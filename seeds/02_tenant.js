590757907cab955c88fdf978231f99f0cdc5308f
felix-soft <felixsoft.dev@gmail.com>
2018-11-15 23:25:47 +0100

    seed fix to not include dwollaUri for tenant

--- seeds/02_tenant.js
+++ seeds/02_tenant.js
@@ -7,14 +7,12 @@ exports.seed = function (knex, Promise) {
                 {
                     id: '7bc0447a-ea99-4ba2-93bb-c84f5b325c50',
                     name: 'DevTenant',
-                    dwollaUri: 'https://api-sandbox.dwolla.com/funding-sources/24a53f09-99ae-420d-8adc-2793845714b6',
                     createdAt: new Date(),
                     updatedAt: new Date(),
                 },
                 {
                     id: '8e1be740-2152-467b-9c9b-be6dac16e9e1',
                     name: 'EvilTenant',
-                    dwollaUri: 'https://api-sandbox.dwolla.com/funding-sources/24a53f09-99ae-420d-8adc-2793845714b6',
                     createdAt: new Date(),
                     updatedAt: new Date(),
                 },
