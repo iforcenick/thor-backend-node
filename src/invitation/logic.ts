ddd3c3e5ada71c45bb8167937a6f18b3c7e9c980
felix-soft <felixsoft.dev@gmail.com>
2018-11-23 12:23:33 +0100

    ODIN-272: csv import refactor (#226)

--- src/invitation/logic.ts
+++ src/invitation/logic.ts
@@ -8,6 +8,10 @@ import {TenantService} from '../tenant/service';
 import {Config} from '../config';
 import {Logger} from '../logger';
 import {ProfileService} from '../profile/service';
+import * as _ from 'lodash';
+import {Errors} from 'typescript-rest';
+import Joi = require('joi');
+
 const parse = require('csv-parse/lib/sync');
 
 @AutoWired
@@ -18,37 +22,61 @@ export class BatchInvitationsLogic extends Logic {
     @Inject profiles: ProfileService;
     @Inject config: Config;
     @Inject logger: Logger;
+    private emailSchema = Joi.string().email();
 
     async execute(buffer: Buffer): Promise<Array<Invitation>> {
-        const invitations = new Array<Invitation>();
+        const maxRows = this.config.get('invitations.csv.rows');
+        let parsed;
 
-        const parser1 = parse(buffer.toString(), {
-            skip_empty_lines: true,
-        });
-
-        for (let i = 1; i < parser1.length; i++) {
-            const record = parser1[i][0].split(';');
-            if (record[0]) {
-                const invitationPrototype = {
-                    email: record[0],
-                    status: models.Status.pending,
-                    externalId: ''
-                };
-                if (record[1]) {
-                    invitationPrototype.externalId = record[1];
-                }
-                invitations.push(Invitation.factory(invitationPrototype));
+        try {
+            parsed = parse(buffer, {
+                skip_empty_lines: true,
+                skip_lines_with_empty_values: true,
+                trim: true,
+                cast: true,
+                delimiter: ';',
+                from: 1,
+                to: maxRows
+            });
+        } catch (e) {
+            if (_.isNumber(e)) { // to many lines in file, crappy reporting
+                throw new Errors.ConflictError(`CSV file has to many rows, max allowed: ${maxRows}`);
             }
+
+            throw e;
         }
-        const emails = invitations.map(invitation => {
-            return invitation.email;
+
+        const invitations = parsed.map((row) => {
+            const invitation = {
+                email: row[0],
+                status: models.Status.pending,
+                externalId: ''
+            };
+
+            if (row[1]) {
+                invitation.externalId = row[1];
+            }
+
+            return invitation;
         });
+
+        const emails = invitations.map((invitation) => {
+            return invitation.email;
+        }).filter((email) => {
+            return Joi.validate(email, this.emailSchema).error === null;
+        });
+
+        if (_.isEmpty(emails)) {
+            throw new Errors.NotAcceptableError('No emails to import');
+        }
+
         await this.checkEmailsDuplicates(emails);
         await this.checkExistingUsers(emails);
         if (invitations.some(invitation => invitation.externalId !== '')) {
             const externalIds = invitations.map(invitation => {
                 return invitation.externalId;
             });
+
             await this.checkExternalIdsDuplicates(externalIds);
             await this.checkRegisteredUsersDuplicates(externalIds);
         }
@@ -58,7 +86,7 @@ export class BatchInvitationsLogic extends Logic {
         try {
             for (const invitation of invitations) {
                 invitation.status = models.Status.sent;
-                await this.invitations.insert(invitation)
+                await this.invitations.insert(invitation);
 
                 this.mailer.sendInvitation(invitation.email, {
                     link: `${this.config.get('application.frontUri')}/on-boarding/${invitation.id}`,
@@ -75,13 +103,9 @@ export class BatchInvitationsLogic extends Logic {
         const result = await this.invitations.getByEmails(emails);
 
         if (result.length > 0) {
-            let errorMessage = 'Emails: ';
-            for (const invitation of result) {
-                errorMessage += `${invitation.email} `;
-            }
-            errorMessage += ' are already invited';
-
-            throw new Error(errorMessage);
+            return this.parseError('Emails already invited', result.map((inv) => {
+                return inv.email;
+            }));
         }
     }
 
@@ -89,13 +113,9 @@ export class BatchInvitationsLogic extends Logic {
         const profiles = await this.profiles.getByEmails(emails);
 
         if (profiles.length > 0) {
-            let errorMessage = 'Emails: ';
-            for (const invitation of profiles) {
-                errorMessage += `${invitation.email} `;
-            }
-            errorMessage += ' are already used';
-
-            throw new Error(errorMessage);
+            return this.parseError('Emails already registered', profiles.map((prof) => {
+                return prof.email;
+            }));
         }
     }
 
@@ -103,28 +123,29 @@ export class BatchInvitationsLogic extends Logic {
         const duplicates = await this.invitations.getByExternalIds(externalIds);
 
         if (duplicates.length > 0) {
-            let errorMessage = 'External ids: ';
-            for (const invitation of duplicates) {
-                errorMessage += `${invitation.externalId} `;
-            }
-            errorMessage += ' are already used';
-
-            throw new Error(errorMessage);
+            return this.parseError('ExternalIds already invited', duplicates.map((inv) => {
+                return inv.email;
+            }));
         }
     }
 
     private async checkRegisteredUsersDuplicates(externalIds: Array<string>) {
-
         const profiles = await this.profiles.getByExternalIds(externalIds);
 
         if (profiles.length > 0) {
-            let errorMessage = 'External ids: ';
-            for (const profile of profiles) {
-                errorMessage += `${profile.externalId} `;
-            }
-            errorMessage += ' are already used';
-
-            throw new Error(errorMessage);
+            return this.parseError('ExternalIds already registered', profiles.map((prof) => {
+                return prof.email;
+            }));
         }
+    }
+
+    private parseError(status: string, items: Array<string>) {
+        const e: any = new Errors.ConflictError();
+        e.message = {
+            'status': status,
+            'items': items,
+        };
+
+        throw e;
     }
 }
